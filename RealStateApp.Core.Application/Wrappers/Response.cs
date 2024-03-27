727f2a2c52a874e09757644f77af1c35e4bec824
Cleimer ANT <cleimer2020@gmail.com>
2024-03-27 05:19:32 +0400

    cambios

---
+++ RealStateApp.Core.Application/Wrappers/Response.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealStateApp.Core.Application.Wrappers
+{
+    public class Response<T>
+    {
+        public bool Suceded { get; set; }
+        public string? Message { get; set; }
+        public List<string> Errors { get; set; }
+        public T Data { get; set; }
+
+        public Response() { }
+        public Response(T data, string message = null)
+        {
+            Suceded = true;
+            Message = message ?? "";
+            Data = data;
+        }
+        public Response(string? message)
+        {
+            Suceded = false;
+            Message = message;
+        }
+    }
+}
