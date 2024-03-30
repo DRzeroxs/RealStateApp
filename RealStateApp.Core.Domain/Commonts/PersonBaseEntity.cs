0446d8d9239a171ad49a498e6e4184b8fe98f9df
DRzeroxs <mgrullonreinoso@gmail.com>
2024-03-30 18:37:33 -0400

    Agregar Correo a AppUsers

--- RealStateApp.Core.Domain/Commonts/PersonBaseEntity.cs
+++ RealStateApp.Core.Domain/Commonts/PersonBaseEntity.cs
@@ -10,6 +10,8 @@ namespace RealStateApp.Core.Domain.Commonts
     public class PersonBaseEntity : AuditableBaseEntity
     {
         public string IdentityId { get; set; }
+
+        public string Correo { get; set; }
         public string? Nombre { get; set; }
         public string? Apellido { get; set; }
 
