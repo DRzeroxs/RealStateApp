3963f9194aefc11e5129e8733b868668aab189d3
Miguel Grullón Reinoso <Mgrullonreinoso@gmail.com>
2024-04-03 21:07:51 -0400

    Arreglo de cambios #1

--- RealStateApp.Core.Application/ViewModel/User/UserPostViewModel.cs
+++ RealStateApp.Core.Application/ViewModel/User/UserPostViewModel.cs
@@ -18,11 +18,5 @@ namespace RealStateApp.Core.Application.ViewModel.User
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }   
-        public string Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Correo { get; set; }
-        public string ImgUrl { get; set; }
-        public bool IsActive { get; set; }
     }
 }
