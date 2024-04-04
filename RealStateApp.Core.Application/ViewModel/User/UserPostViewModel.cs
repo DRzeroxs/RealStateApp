52f9b89ee761bb0c53d9e6eba3a5b255cbb21429
Miguel Grullón Reinoso <Mgrullonreinoso@gmail.com>
2024-04-03 20:57:47 -0400

    Intento #1 de solucionar Merge de funcionalidades a prueba

--- RealStateApp.Core.Application/ViewModel/User/UserPostViewModel.cs
+++ RealStateApp.Core.Application/ViewModel/User/UserPostViewModel.cs
@@ -9,7 +9,7 @@ namespace RealStateApp.Core.Application.ViewModel.User
 {
     public class UserPostViewModel
     {
-        public string UserId { get; set; }  
+        public string UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
@@ -17,12 +17,6 @@ namespace RealStateApp.Core.Application.ViewModel.User
         public string? ImgUrl { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public string Email { get; set; }   
-        public string Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Correo { get; set; }
-        public string ImgUrl { get; set; }
-        public bool IsActive { get; set; }
+        public string Email { get; set; }
     }
 }
