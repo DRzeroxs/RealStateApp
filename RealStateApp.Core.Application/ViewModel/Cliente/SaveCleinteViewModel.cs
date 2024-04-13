e9baa39593cd405307d3f084a5555a41f7cf4202
Cleimer ANT <cleimer2020@gmail.com>
2024-04-13 23:31:17 +0400

    agregar agente y cliente al otro esquema

--- RealStateApp.Core.Application/ViewModel/Cliente/SaveCleinteViewModel.cs
+++ RealStateApp.Core.Application/ViewModel/Cliente/SaveCleinteViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RealStateApp.Core.Application.ViewModel.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,13 @@ using System.Threading.Tasks;
 
 namespace RealStateApp.Core.Application.ViewModel.Cliente
 {
-    public class SaveCleinteViewModel
+    public class SaveCleinteViewModel : BasePersonSaveViewModel
     {
         public string IdentityId { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Cedula { get; set; }
         public string Correo { get; set; }
+
     }
 }
